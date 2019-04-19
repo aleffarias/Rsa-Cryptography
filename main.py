f3394c166c0c0072ca3c6430fc51ba105f163229
aleffarias <aaof@ic.ufal.br>
2019-04-19 17:44:08 -0300

    Atualizando menu
    Usuário pode gerar as chaves automaticamente ou digita-las

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from rsa import Rsa
 
-print('\tRSA ALGORITHM\n')
+print('\n\t* RSA ALGORITHM *')
 
 while True:
     option = input('\nEscolha uma opção:\n(1) - Gerar chaves\n(2) - Criptografar\n(3) - Descriptografar\n(0) - Sair\n')
     if (option == '1'):
-        generate_key = input('Deseja gerar a chave púbica e privada?\n(1) Sim - Gerar automaticamente\n(2) Não - Digitar as chaves\n')
+        generate_key = input('\nDeseja gerar a chave púbica e a chave privada automaticamente?\n(1) Sim - Gerar automaticamente\n(2) Não - Digitar as chaves\n')
         if (generate_key == '1'):
             print('\nGerando as chaves...')
             Rsa.setup(1)
         elif (generate_key == '2'):
-            Rsa.setup(0)
+            Rsa.setup(2)
         else:
             print('Erro! Opção Inválida.\n')
 
     elif (option == '2'):
         message = input('Digite a mensagem que será criptografada!\n')
-        print('\nLendo a chave pública...\nCriptografando a menssagem...')
+        print('\nLendo a chave pública no arquivo public_keys.txt...\nCriptografando a menssagem...')
         Rsa.encrypt(message)
 
     elif (option == '3'):
-        encry_message = input('Digite ou copia a mensagem que será descriptografada separada por , e sem []\nExemplo: [10 , 20, 30] -> 10, 11, 30\n')
-        print('Lendo a chave privada')
-        print('\nMenssagem descriptografada: ', Rsa.decrypt(encry_message))
+        encry_message = input('Digite ou copie a mensagem que será descriptografada separada por , e sem []\nExemplo: [10, 20, 30] -> 10, 11, 30\n')
+
+        print('Lendo a chave privada no arquivo private_keys.txt...')
+        print('\nMenssagem descriptografada: ', Rsa.decrypt(check, encry_message))
 
     elif (option == '0'):
-        sys.exit()
+        exit()
