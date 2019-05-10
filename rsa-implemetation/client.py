a6b01e3e26c83bf1ff36a1446f55f0935f346001
aleffarias <aaof@ic.ufal.br>
2019-05-10 01:16:57 -0300

    Adicionando método add

--- rsa-implemetation/client.py
+++ rsa-implemetation/client.py
@@ -1,7 +1,19 @@
 from rsa import Rsa
 
-class client(object):
+class Client(object):
     ''' Classe onde é fieta a adição, deleção e verificação dos usuários '''
 
     def add():
-        
+        i = 0
+        while True:
+            # add condição caso exista
+            user = input('Digite o nome do seu usuário: ')
+            password = input('Digite uma senha: ')
+
+            print("Usuário cadastrado\n")
+
+            f_add = open('files/users.txt', 'w')
+            f_add.write(str(user) + ' | ' + str(password) + '\n')
+            f_add.close()
+
+Client.add()
